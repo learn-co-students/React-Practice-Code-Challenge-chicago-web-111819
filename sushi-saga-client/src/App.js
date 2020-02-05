919cc521dbad4324c46e781eef6292c72ba97c7f
porawiec <54656651+porawiec@users.noreply.github.com>
2020-02-04 22:25:59 -0600

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,69 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  
+    constructor(){
+      super()
+  
+      this.state = {
+        sushis: [],
+        consumed: [],
+        wallet: 100,
+        displayIndex: 0
+      }
+    }
+  
+  componentDidMount(){
+    fetch(API)
+    .then(resp => resp.json())
+    .then(sushisObj => {
+      // console.log(sushisObj)
+      this.setState({
+        sushis: sushisObj
+      })
+    })
+  }
 
+  fourSushis = () => {
+    return this.state.sushis.slice(this.state.displayIndex, this.state.displayIndex + 4)
+  }
+
+  moreBtnLogic = (event) => {
+    let newDisplayIndex = this.state.displayIndex + 4 
+
+    if(newDisplayIndex >= this.state.sushis.length){
+      newDisplayIndex = 0
+    }
+
+    this.setState({
+      displayIndex: newDisplayIndex
+    })
+  }
+
+  consumeFcn = (sushi) => {
+    const newWallet = this.state.wallet - sushi.price
+
+    if(!this.state.consumed.includes(sushi) && newWallet >=0) {
+      this.setState({
+        consumed: [...this.state.consumed, sushi],
+        wallet: newWallet
+      })
+    }
+  }
+  
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer
+          sushis={this.fourSushis()}
+          moreBtnLogic={this.moreBtnLogic}
+          consumed={this.state.consumed}
+          consumeFcn={this.consumeFcn}
+        />
+        <Table 
+          wallet={this.state.wallet}
+          consumed={this.state.consumed}
+        />
       </div>
     );
   }
